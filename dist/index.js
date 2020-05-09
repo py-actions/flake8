92131c34162fe252f2af66521637e8674bc2c836
Chris Simpkins <git.simpkins@gmail.com>
2020-05-09 17:17:18 -0400

    new dist build

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(104);
+/******/ 		return __webpack_require__(676);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -954,79 +954,6 @@ module.exports = require("os");
 
 /***/ }),
 
-/***/ 104:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-const core = __webpack_require__(470);
-const exec = __webpack_require__(986);
-
-async function run() {
-  const sourcePath = core.getInput("path");
-  const updatePip = core.getInput("update-pip");
-  const ignoreRuleCodes = core.getInput("ignore");
-  const excludePaths = core.getInput("exclude");
-  const maxLineLength = core.getInput("max-line-length");
-  const flake8Args = core.getInput("args");
-  const flake8Version = core.getInput("flake8-version");
-
-  // ====================
-  // Install dependencies
-  // ====================
-  try {
-    // update pip
-    if (updatePip === "true") {
-      console.log("[*] Updating pip package...");
-      await exec.exec("python -m pip install --upgrade pip");
-    }
-
-    // install/update flake8 package
-    console.log(`[*] Installing flake8 package @ ${flake8Version}...`);
-    if (flake8Version === "latest") {
-      await exec.exec("python -m pip install --upgrade flake8");
-    } else if (flake8Version === "master") {
-      await exec.exec(
-        "python -m pip install --upgrade git+https://gitlab.com/pycqa/flake8.git"
-      );
-    } else {
-      await exec.exec(
-        `python -m pip install --upgrade flake8==${flake8Version}`
-      );
-    }
-    // show installed flake8 version that will be used during the tests
-    console.log("[*] Installed flake8 package version:");
-    await exec.exec("flake8 --version");
-
-    // prep flake8 command for execution
-    let flake8Cmd = "flake8";
-    if (ignoreRuleCodes !== "none") {
-      flake8Cmd += ` --ignore ${ignoreRuleCodes}`;
-    }
-    if (excludePaths !== "none") {
-      flake8Cmd += ` --exclude ${excludePaths}`;
-    }
-    if (maxLineLength !== "none") {
-      flake8Cmd += ` --max-line-length ${maxLineLength}`;
-    }
-    if (flake8Args !== "none") {
-      flake8Cmd += ` ${flake8Args}`;
-    }
-    // concatenate test path
-    flake8Cmd += ` ${sourcePath}`;
-
-    // execute flake8
-    await exec.exec(`${flake8Cmd}`);
-  } catch (error) {
-    core.setFailed(
-      `ERROR: Action failed during execution with error: ${error.message}`
-    );
-  }
-}
-
-run();
-
-
-/***/ }),
-
 /***/ 129:
 /***/ (function(module) {
 
@@ -1589,6 +1516,79 @@ function isUnixExecutable(stats) {
         ((stats.mode & 64) > 0 && stats.uid === process.getuid()));
 }
 //# sourceMappingURL=io-util.js.map
+
+/***/ }),
+
+/***/ 676:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const core = __webpack_require__(470);
+const exec = __webpack_require__(986);
+
+async function run() {
+  const sourcePath = core.getInput("path");
+  const updatePip = core.getInput("update-pip");
+  const ignoreRuleCodes = core.getInput("ignore");
+  const excludePaths = core.getInput("exclude");
+  const maxLineLength = core.getInput("max-line-length");
+  const flake8Args = core.getInput("args");
+  const flake8Version = core.getInput("flake8-version");
+
+  // ====================
+  // Install dependencies
+  // ====================
+  try {
+    // update pip
+    if (updatePip === "true") {
+      console.log("[*] Updating pip package...");
+      await exec.exec("python -m pip install --upgrade pip");
+    }
+
+    // install/update flake8 package
+    console.log(`[*] Installing flake8 package @ ${flake8Version}...`);
+    if (flake8Version === "latest") {
+      await exec.exec("python -m pip install --upgrade flake8");
+    } else if (flake8Version === "master") {
+      await exec.exec(
+        "python -m pip install --upgrade git+https://gitlab.com/pycqa/flake8.git"
+      );
+    } else {
+      await exec.exec(
+        `python -m pip install --upgrade flake8==${flake8Version}`
+      );
+    }
+    // show installed flake8 version that will be used during the tests
+    console.log("[*] Installed flake8 package version:");
+    await exec.exec("flake8 --version");
+
+    // prep flake8 command for execution
+    let flake8Cmd = "flake8";
+    if (ignoreRuleCodes !== "none") {
+      flake8Cmd += ` --ignore ${ignoreRuleCodes}`;
+    }
+    if (excludePaths !== "none") {
+      flake8Cmd += ` --exclude ${excludePaths}`;
+    }
+    if (maxLineLength !== "none") {
+      flake8Cmd += ` --max-line-length ${maxLineLength}`;
+    }
+    if (flake8Args !== "none") {
+      flake8Cmd += ` ${flake8Args}`;
+    }
+    // concatenate test path
+    flake8Cmd += ` ${sourcePath}`;
+
+    // execute flake8
+    await exec.exec(`${flake8Cmd}`);
+  } catch (error) {
+    core.setFailed(
+      `ERROR: Action failed during execution with error: ${error.message}`
+    );
+  }
+}
+
+run();
+
 
 /***/ }),
 
