02d4c833f141987ee59c13a6a5829fc466132793
Chris Simpkins <git.simpkins@gmail.com>
2020-05-09 16:31:55 -0400

    add new test file

---
+++ tests/test-args.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+def main():
+    sys.stdout.write(
+        "Hello world!  This is an exceedingly long line that exceeds the max limits."
+    )
+
+
+if __name__ == "__main__":
+    main()
