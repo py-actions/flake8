daac069a2e58ae3fe85ac97efbe750d5a9d835c7
rickstaa <rick.staa@outlook.com>
2020-12-10 14:12:03 +0100

    :rocket: Updates distribution package

--- dist/index.js
+++ dist/index.js
@@ -1605,6 +1605,10 @@ async function run() {
   const flake8Args = core.getInput("args");
   const flake8Version = core.getInput("flake8-version");
 
+  const github_token = core.getInput("github_token");
+  const level = core.getInput("level");
+  const reporter = core.getInput("reporter");
+
   // ====================
   // Install dependencies
   // ====================
@@ -1614,6 +1618,12 @@ async function run() {
       console.log("[*] Updating pip package...");
       await exec.exec("python -m pip install --upgrade pip");
     }
+
+    // install reviewdog
+    console.log(`[*] Installing reviewdog...`);
+    await exec.exec(
+      `/bin/bash -c "wget -O - -q https://raw.githubusercontent.com/reviewdog/nightly/master/install.sh| sh -s -- -b /usr/local/bin/`
+    );
 
     // install/update flake8 package
     console.log(`[*] Installing flake8 package @ ${flake8Version}...`);
@@ -1649,7 +1659,27 @@ async function run() {
     // concatenate test path
     flake8Cmd += ` ${sourcePath}`;
 
+    // Export github token
+    console.log(`[*] Setting github api token as env variable...`);
+    process.env["REVIEWDOG_GITHUB_API_TOKEN"] = github_token;
+
+    // Validate reviewdog input arguments
+    if (reporter === "github-pr-review") {
+      throw new Error("github-pr-review unsupported (for now)");
+    }
+    if ((reporter === "None") | (reporter === "")) {
+      reporter = "github-pr-check";
+    }
+    if ((level === "None") | (reporter === "")) {
+      level = "error";
+    }
+
+    // Add reviewdog execution code
+    console.log(`[*] Adding reviewdog command...`);
+    flake8Cmd += `| reviewdog -f flake8 -name="flake8-lint" -reporter="${reporter}" -level="${level}" -tee`;
+
     // execute flake8
+    console.log(`[*] Executing flake8 + reviewdog command...`);
     await exec.exec(`${flake8Cmd}`);
   } catch (error) {
     core.setFailed(
