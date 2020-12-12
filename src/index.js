7988c4250180359dee0a579972ce93d07f7490a5
Rick Staa <rick.staa@outlook.com>
2020-12-12 14:11:49 +0100

    :bug: Removes debug commands

--- src/index.js
+++ src/index.js
@@ -38,114 +38,105 @@ async function run() {
       await exec.exec("python -m pip install --upgrade pip");
     }
 
-    // // install Reviewdog (nightly)
-    // console.log(`[*] Installing reviewdog...`);
-    // if (process.platform === "win32") {
-    //   const semver = REVIEWDOG_VERSION.substring(1);
-    //   const downloadUrl = `https://github.com/reviewdog/nightly/releases/download/${REVIEWDOG_VERSION}/reviewdog_${semver}_Windows_x86_64.tar.gz`;
-    //   const outputPath = path.resolve(
-    //     path.join(gh_ws_path, "reviewdog.tar.gz")
-    //   );
-    //   console.log(`[*] Output path: ${outputPath}`);
-    //   console.log(`[*] test`);
-    //   await exec.exec(`curl -LJ ${downloadUrl} -o ${outputPath}`);
-    //   await exec.exec(`tar -xkvzf ${outputPath}`);
-    // } else {
-    //   await exec.exec(
-    //     `/bin/bash -c "wget -O - -q https://raw.githubusercontent.com/reviewdog/nightly/master/install.sh| sudo sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}`
-    //   ); // /bin/bash -c is needed since @actions/exec does not yet support piping https://github.com/actions/toolkit/issues/359
-    // }
+    // install Reviewdog (nightly)
+    console.log(`[*] Installing reviewdog...`);
+    if (process.platform === "win32") {
+      const semver = REVIEWDOG_VERSION.substring(1);
+      const downloadUrl = `https://github.com/reviewdog/nightly/releases/download/${REVIEWDOG_VERSION}/reviewdog_${semver}_Windows_x86_64.tar.gz`;
+      const outputPath = path.resolve(
+        path.join(gh_ws_path, "reviewdog.tar.gz")
+      );
+      console.log(`[*] Output path: ${outputPath}`);
+      console.log(`[*] test`);
+      await exec.exec(`curl -LJ ${downloadUrl} -o ${outputPath}`);
+      await exec.exec(`tar -xkvzf ${outputPath}`);
+    } else {
+      await exec.exec(
+        `/bin/bash -c "wget -O - -q https://raw.githubusercontent.com/reviewdog/nightly/master/install.sh| sudo sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}`
+      ); // /bin/bash -c is needed since @actions/exec does not yet support piping https://github.com/actions/toolkit/issues/359
+    }
 
-    // // install dependencies if user requested this
-    // if (installDeps !== "none" && installDeps) {
-    //   let devArg = "";
-    //   if (devInstall !== "none" && devInstall) {
-    //     devArg = "-e";
-    //   }
-    //   let reqFilePathArg = "requirements.txt";
-    //   if (reqFilePath !== "none") {
-    //     reqFilePathArg = reqFilePath;
-    //   }
-    //   try {
-    //     if (fs.existsSync("setup.py")) {
-    //       console.log(`[*] Installing python package and dependencies...`);
-    //       await exec.exec(`python -m pip install ${devArg} .`);
-    //     } else if (fs.existsSync("requirements.txt")) {
-    //       console.log(`[*] Installing package dependencies...`);
-    //       await exec.exec(`python -m pip install -r ${reqFilePathArg}`);
-    //     } else {
-    //       console.log(
-    //         `[*] Installing package dependencies failed as no 'setup.py' or 'requirements.txt' was found.`
-    //       );
-    //     }
-    //   } catch (error) {
-    //     console.log(`[*] Installing package dependencies failed.`);
-    //   }
-    // }
+    // install dependencies if user requested this
+    if (installDeps !== "none" && installDeps) {
+      let devArg = "";
+      if (devInstall !== "none" && devInstall) {
+        devArg = "-e";
+      }
+      let reqFilePathArg = "requirements.txt";
+      if (reqFilePath !== "none") {
+        reqFilePathArg = reqFilePath;
+      }
+      try {
+        if (fs.existsSync("setup.py")) {
+          console.log(`[*] Installing python package and dependencies...`);
+          await exec.exec(`python -m pip install ${devArg} .`);
+        } else if (fs.existsSync("requirements.txt")) {
+          console.log(`[*] Installing package dependencies...`);
+          await exec.exec(`python -m pip install -r ${reqFilePathArg}`);
+        } else {
+          console.log(
+            `[*] Installing package dependencies failed as no 'setup.py' or 'requirements.txt' was found.`
+          );
+        }
+      } catch (error) {
+        console.log(`[*] Installing package dependencies failed.`);
+      }
+    }
 
-    // // install/update flake8 package
-    // console.log(`[*] Installing flake8 package @ ${flake8Version}...`);
-    // if (flake8Version === "latest") {
-    //   await exec.exec("python -m pip install --upgrade flake8");
-    // } else if (flake8Version === "master") {
-    //   await exec.exec(
-    //     "python -m pip install --upgrade git+https://gitlab.com/pycqa/flake8.git"
-    //   );
-    // } else {
-    //   await exec.exec(
-    //     `python -m pip install --upgrade flake8==${flake8Version}`
-    //   );
-    // }
-    // // show installed flake8 version that will be used during the tests
-    // console.log("[*] Installed flake8 package version:");
-    // await exec.exec("flake8 --version");
+    // install/update flake8 package
+    console.log(`[*] Installing flake8 package @ ${flake8Version}...`);
+    if (flake8Version === "latest") {
+      await exec.exec("python -m pip install --upgrade flake8");
+    } else if (flake8Version === "master") {
+      await exec.exec(
+        "python -m pip install --upgrade git+https://gitlab.com/pycqa/flake8.git"
+      );
+    } else {
+      await exec.exec(
+        `python -m pip install --upgrade flake8==${flake8Version}`
+      );
+    }
+    // show installed flake8 version that will be used during the tests
+    console.log("[*] Installed flake8 package version:");
+    await exec.exec("flake8 --version");
 
-    // // prep flake8 command for execution
-    // let flake8Cmd = "flake8";
-    // if (ignoreRuleCodes !== "none") {
-    //   flake8Cmd += ` --ignore ${ignoreRuleCodes}`;
-    // }
-    // if (excludePaths !== "none") {
-    //   flake8Cmd += ` --exclude ${excludePaths}`;
-    // }
-    // if (maxLineLength !== "none") {
-    //   flake8Cmd += ` --max-line-length ${maxLineLength}`;
-    // }
-    // if (flake8Args !== "none") {
-    //   flake8Cmd += ` ${flake8Args}`;
-    // }
-    // // concatenate test path
-    // flake8Cmd += ` ${sourcePath}`;
+    // prep flake8 command for execution
+    let flake8Cmd = "flake8";
+    if (ignoreRuleCodes !== "none") {
+      flake8Cmd += ` --ignore ${ignoreRuleCodes}`;
+    }
+    if (excludePaths !== "none") {
+      flake8Cmd += ` --exclude ${excludePaths}`;
+    }
+    if (maxLineLength !== "none") {
+      flake8Cmd += ` --max-line-length ${maxLineLength}`;
+    }
+    if (flake8Args !== "none") {
+      flake8Cmd += ` ${flake8Args}`;
+    }
+    // concatenate test path
+    flake8Cmd += ` ${sourcePath}`;
 
-    // // Validate reviewdog input arguments
-    // let reporterArg = "github-pr-check";
-    // if (reporter !== "None") {
-    //   reporterArg = reporter;
-    // }
-    // let levelArg = "error";
-    // if (level !== "None") {
-    //   levelArg = level;
-    // }
-
-    // setup reviewdog execution command
-    console.log(`[*] Adding reviewdog command...`);
-    const flake8Cmd = "flake8";
-    const reporterArg = "github-pr-check";
-    const levelArg = "error";
+    // Validate reviewdog input arguments
+    let reporterArg = "github-pr-check";
+    if (reporter !== "None") {
+      reporterArg = reporter;
+    }
+    let levelArg = "error";
+    if (level !== "None") {
+      levelArg = level;
+    }
 
     // execute flake8 with reviewdog annotations
     console.log(`[*] Executing flake8 + reviewdog command...`);
     if (process.platform === "win32") {
-      const reviewdogCmd = `reviewdog.exe -f flake8 -name=flake8-lint -reporter=${reporterArg} -level=${levelArg} -tee`;
+      const reviewdogExe = path.resolve(path.join(gh_ws_path, "reviewdog.exe"));
+      const reviewdogCmd = `${reviewdogExe} -f flake8 -name=flake8-lint -reporter=${reporterArg} -level=${levelArg} -tee`;
       await exec.exec(`cmd /C "python -m ${flake8Cmd} | ${reviewdogCmd}"`, [], {
         env: {
           REVIEWDOG_GITHUB_API_TOKEN: githubToken,
         },
       });
-      // await exec.exec(`set`);
-      // await exec.exec(`${flake8Cmd}|${reviewdogCmd}`, {
-      //   env: { REVIEWDOG_GITHUB_API_TOKEN: `${githubToken}` },
-      // });
     } else {
       const reviewdogCmd = `reviewdog -f flake8 -name="flake8-lint" -reporter="${reporterArg}" -level="${levelArg}" -tee`;
       await exec.exec(
